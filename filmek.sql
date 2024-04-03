b0b02b3cbfcbaf479bcc6b117ea0861bddc5ac5f
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2024-04-03 19:36:39 +0200

    emgser

--- filmek.sql
+++ filmek.sql
@@ -1,6 +1,28 @@
-CREATE TABLE filmek(teszt TEXT);
+CREATE TABLE filmek(kod TEXT, nev TEXT, leiras TEXT, kep1 TEXT, kep2 TEXT, kep3 TEXT, kep4 TEXT, kep5 TEXT, kep6 TEXT, fokep TEXT);
 
-INSERT INTO filmek(teszt) VALUES("teszt");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("PeWnAFeqMUA", "Elemi", "A Disney és a Pixar új filmje, az ELEMI egy olyan városban játszódik, ahol egymás mellett élnek a tűz-, víz-, föld- és levegőlakók. Parázs, a talpraesett, gyors észjárású és tüzes fiatal nő, illetve Örvény, a mókás, életvidám, laza srác találkozása alapjaiban változatatja meg mindazt, amit az őket körülvevő világról hittek.", "https://media.port.hu/images/001/575/094.webp", "https://media.port.hu/images/001/575/096.webp", "https://media.port.hu/images/001/575/098.webp", "https://s3.us-east-1.amazonaws.com/world-website-storage/wng-prod/_1500x937_crop_center-center_82_line/1774087/elemental13.jpg", "https://www.columbian.com/wp-content/uploads/2023/06/Film_Review_-_Elemental_55099-315ec.jpg", "https://media.npr.org/assets/img/2023/06/12/elemental-online-use-m551_11_pub.pub16.158_custom-7ec940496b02b4a347356a2de3f45dd612805184.jpg", "https://sm.ign.com/ign_hu/screenshot/default/elemental-ver10-xxlg-crop_56g8.jpg");
+
+
+
+
+
+
+
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
+
+
 
 
 
