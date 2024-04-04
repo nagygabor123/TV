09da39b2301273c6dafee1e0525a2e786359f495
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2024-04-04 12:53:49 +0200

    dgosfgisdgsdghrtheHETHeth

--- filmek.sql
+++ filmek.sql
@@ -2,18 +2,6 @@ CREATE TABLE filmek(kod TEXT, nev TEXT, leiras TEXT, kep1 TEXT, kep2 TEXT, kep3 
 
 INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("PeWnAFeqMUA", "Elemi", "A Disney és a Pixar új filmje, az ELEMI egy olyan városban játszódik, ahol egymás mellett élnek a tűz-, víz-, föld- és levegőlakók. Parázs, a talpraesett, gyors észjárású és tüzes fiatal nő, illetve Örvény, a mókás, életvidám, laza srác találkozása alapjaiban változatatja meg mindazt, amit az őket körülvevő világról hittek.", "https://media.port.hu/images/001/575/094.webp", "https://media.port.hu/images/001/575/096.webp", "https://media.port.hu/images/001/575/098.webp", "https://s3.us-east-1.amazonaws.com/world-website-storage/wng-prod/_1500x937_crop_center-center_82_line/1774087/elemental13.jpg", "https://www.columbian.com/wp-content/uploads/2023/06/Film_Review_-_Elemental_55099-315ec.jpg", "https://media.npr.org/assets/img/2023/06/12/elemental-online-use-m551_11_pub.pub16.158_custom-7ec940496b02b4a347356a2de3f45dd612805184.jpg", "https://sm.ign.com/ign_hu/screenshot/default/elemental-ver10-xxlg-crop_56g8.jpg");
 INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("1mR3r0JkJDI", "Oppenheimer", "A film J. Robert Oppenheimer fizikusról szól, aki kollégáival - köztük a magyar származású Teller Edével és Szilárd Leóval - a Manhattan Terv vezetőjeként az első atomfegyver létrehozásáért felelős. A találmány a II. világháborúban megváltoztatta a történelem menetét.", "https://media.port.hu/images/001/581/442.webp", "https://media.port.hu/images/001/581/440.webp", "https://media.port.hu/images/001/581/444.webp", "https://media.port.hu/images/001/580/288.webp", "https://media.port.hu/images/001/580/290.webp", "https://media.port.hu/images/001/553/656.webp", "https://ma7media.storage.googleapis.com/sites/default/files/styles/freeform_large_9_2x/s3/2023-08/oppenheimer-cimlap.png.jpg?h=920929c4&itok=N-I4qz3T");
-INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
-INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
-INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
-INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
-INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
-INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
-INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
-INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
-INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
-INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
-INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
-INSERT INTO filmek(kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep) VALUES("", "", "", "", "", "", "", "", "", "");
 
 
 
---
+++ mafilm/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mafilm'
+});
+
+connection.connect();
+
+// Statikus fájlok kiszolgálása (például az index.html)
+app.use(express.static('public'));
+
+
+
+// Útvonal a videók lekérdezéséhez
+app.get('/videos', (req, res) => {
+    connection.query('SELECT kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep FROM filmek', (error, results) => {
+        if (error) throw error;
+        const videos = results.map(result => ({
+            kod: result.kod,
+            nev: result.nev,
+            leiras: result.leiras,
+            kep1: result.kep1,
+            kep2: result.kep2,
+            kep3: result.kep3,
+            kep4: result.kep4,
+            kep5: result.kep5,
+            kep6: result.kep6,
+            fokep: result.fokep
+        }));
+        res.json(videos); // Az adatok JSON formátumban kerülnek elküldésre
+    });
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`A szerver fut a http://localhost:${port} címen`);
+});
