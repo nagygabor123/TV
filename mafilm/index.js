e248bc5d65b6ae038f53bd7ef6a232061e706808
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2024-04-07 09:22:38 +0200

    sedgsdfg

--- mafilm/index.js
+++ mafilm/index.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'tv'
+    database: 'mafilm'
 });
 
 connection.connect();
@@ -18,10 +18,10 @@ app.use(express.static('public'));
 
 // Útvonal a videók lekérdezéséhez
 app.get('/videos', (req, res) => {
-    connection.query('SELECT cim, ytlink FROM youtube_videos', (error, results) => {//'SELECT kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep FROM filmek'
+    connection.query('SELECT kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep FROM filmek', (error, results) => {//'SELECT kod, nev, leiras, kep1, kep2, kep3, kep4, kep5, kep6, fokep FROM filmek'
         if (error) throw error;
         const videos = results.map(result => ({
-            /*kod: result.kod,
+            kod: result.kod,
             nev: result.nev,
             leiras: result.leiras,
             kep1: result.kep1,
@@ -30,9 +30,9 @@ app.get('/videos', (req, res) => {
             kep4: result.kep4,
             kep5: result.kep5,
             kep6: result.kep6,
-            fokep: result.fokep*/
-            cim: result.cim,
-            link:result.ytlink
+            fokep: result.fokep
+            /*cim: result.cim,
+            link:result.ytlink*/
 
         }));
         res.json(videos); // Az adatok JSON formátumban kerülnek elküldésre
